abda61c83819d2388143eb54a675d82d2e03abaf
lhamutserings <lhamu.sherpa.us@gmail.com>
2022-11-15 10:29:54 -0500

    more arithmetic

--- src/Sum.java
+++ src/Sum.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Sum {
     public static void main(String[] args) {
-        int a=10;
-        int b=20;
-        int sum=a+b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the first number");
+        int a = sc.nextInt();
+        System.out.println("Please enter the second number");
+        int b = sc.nextInt();
+
+        // performing arithmetic operations
+
+        int sum = a + b;
+        int diff = Math.abs(a -b);
+        int product = a*b;
+
         System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(product);
+
     }
 }
